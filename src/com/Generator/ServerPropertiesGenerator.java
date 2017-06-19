afc84c526e4a372b66d4aad6e9805e8ace95fa82
Apex-General <BenN.Business@Gmail.com>
2017-06-19 18:41:51 -0500

    Setup

---
+++ src/com/Generator/ServerPropertiesGenerator.java
@@ -0,0 +1,10 @@
+package com.Generator;
+
+public class ServerPropertiesGenerator {
+
+	public static final class Group0{}
+	public static final class Group1{}
+	public static final class Group2{}
+	public static final class Group4{}
+//Yo
+}
